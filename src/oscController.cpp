a9de87374f07108dd3eabe073a41da01b17514f7
ReallyRad <arthur.pointeau@gmail.com>
2015-02-21 20:32:56 -0500

    hardcode dport

--- src/oscController.cpp
+++ src/oscController.cpp
@@ -8,7 +8,7 @@ void oscController::setup(machine* m, soundPlayer* p){
 	mySoundPlayer = p;	
 	if (myMachine->setup_type == TWO_WAY_SWAP) sender.setup("192.168.1.100", 8015);
 	else if (myMachine->setup_type == ONE_WAY_SWAP) sender.setup("localhost", PORT);	
-	receiver.setup(PORT);    	
+	receiver.setup(8015);    	
 	//tabletOscSender.setup(PHONE_IP, PHONE_SENDER_PORT);    
 }	
 
