1851e39b19b843139acbd094c6b426452d597ed4
ReallyRad <arthur.pointeau@gmail.com>
2015-04-10 16:53:55 +0200

    added xml settings to machine.cpp

--- src/testApp.h
+++ src/testApp.h
@@ -5,6 +5,7 @@
 #include "oscController.h"
 #include "ofxImageSequenceRecorder.h"
 #include "ofxFensterManager.h"
+#include "ofxXmlSettings.h"
 
 #define COMPUTER 1 //computer 1 is in charge of getting input from touchOSC tablet and relaying it to computer 2
 #define CAPTURE_FRAME_RATE 24 //for video recording
