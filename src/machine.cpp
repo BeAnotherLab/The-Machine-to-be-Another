0e9947a1c4b1d47ab0d447513957c547387e5424
ReallyRad <arthur.pointeau@gmail.com>
2015-12-04 16:21:30 +0100

    merge

--- src/machine.cpp
+++ src/machine.cpp
@@ -6,7 +6,20 @@ void machine::setup(ofxXmlSettings * se)
 	hmd = ovrHmd_Create(0);	
 	if (!hmd || !ovrHmd_ConfigureTracking(hmd, ovrTrackingCap_Orientation |ovrTrackingCap_MagYawCorrection |ovrTrackingCap_Position, 
 											   ovrTrackingCap_Orientation |ovrTrackingCap_MagYawCorrection |ovrTrackingCap_Position)) {
-		cout << "Unable to detect Rift head tracker" << endl;		
+		cout << "Unable to detect Oculus Rift" << endl;				
+		fboLeft.allocate(800,600);
+		fboRight.allocate(800,600);
+	} else {
+		if (hmd->Type == ovrHmd_DK2 ) { 
+			cout << "Detected Oculus Rift DK2" << endl;				
+			fboLeft.allocate(DK2_WIDTH/2, DK2_HEIGHT);
+			fboRight.allocate(DK2_WIDTH/2, DK2_HEIGHT);
+		}
+		else if(hmd->Type == ovrHmd_DK1) {
+			cout << "Detected Oculus Rift DK1" << endl;				
+			fboLeft.allocate(DK1_WIDTH/2, DK1_HEIGHT);
+			fboRight.allocate(DK1_WIDTH/2, DK1_HEIGHT);
+		}
 	}
 
 	ovrHmd_RecenterPose(hmd); 
@@ -14,31 +27,29 @@ void machine::setup(ofxXmlSettings * se)
 	settings = se;
 	setup_type = settings->getValue("settings:setup_type", ONE_WAY_SWAP);
 	camera_type = settings->getValue("settings:camera_type", MONO);
-	swapLR = settings->getValue("settings:swapLR", 0);
-	//tested with PS3Eye camera.	
 	ipd = settings->getValue("settings:ipd", 8);
 	camWidth = 1280;
-	camHeight = 720;
-	calibration = 0;
-	
+	camHeight =1024;
+	drift_correction = 0;
+
 	if (camera_type == MONO) {
 		vidGrabberLeft.setVerbose(true);
         vidGrabberLeft.setDeviceID(settings->getValue("settings:camera_id", 1));							
-		vidGrabberLeft.setDesiredFrameRate(120);
-		vidGrabberLeft.initGrabber(camWidth,camHeight);			
+		vidGrabberLeft.setDesiredFrameRate(75);
+		vidGrabberLeft.initGrabber(camWidth, camHeight);			
 		vidGrabberLeft.setAnchorPercent(0.5,0.5);
 	} else if (camera_type == STEREO) {		
 	    vidGrabberRight.setVerbose(true);
 		vidGrabberRight.setDeviceID(settings->getValue("settings:camera_id", 2));
-		vidGrabberRight.setDesiredFrameRate(120);
-		vidGrabberRight.initGrabber(camWidth,camHeight);	
-		vidGrabberRight.setAnchorPercent(0.5,0.5);
+		vidGrabberRight.setDesiredFrameRate(75);
+		vidGrabberRight.initGrabber(camWidth, camHeight);	
+		vidGrabberRight.setAnchorPercent(0.5, 0.5);
 	} else if (camera_type == OVRVISION) {
 		g_pOvrvision = new OVR::Ovrvision();
-	    g_pOvrvision->Open(0,OVR::OV_CAMVGA_FULL);   
-	    left.allocate(camWidth,camHeight,GL_RGB);
-		left.setAnchorPercent(0.5,0.5);
-	    right.allocate(camWidth,camHeight,GL_RGB);
+	    g_pOvrvision->Open(0, OVR::OV_CAMVGA_FULL);   
+	    left.allocate(camWidth, camHeight,GL_RGB);
+		left.setAnchorPercent(0.5, 0.5);
+	    right.allocate(camWidth, camHeight, GL_RGB);
 		right.setAnchorPercent(0.5,0.5);
 	}
 
@@ -47,14 +58,197 @@ void machine::setup(ofxXmlSettings * se)
 	roll = 0;
 	rx_pitch = 0;
 	rx_yaw = 0;
-	rx_roll = 0;
-	pitch_cal = 0;
-	yaw_cal = 0;
-	roll_cal = 0;
+	rx_roll = 0;		
+	
+	fboLeft.setAnchorPercent(0.5, 0.5);
+	fboRight.setAnchorPercent(0.5, 0.5);
+
+	zoom = settings->getValue("settings:zoom", 0.85);
+	speed = settings->getValue("settings:speed", 920);
+	alignment = settings->getValue("settings:alignment", 0);
+	ipd = settings->getValue("settings:ipd", 8);	
+	orientation = settings->getValue("settings:camera_orientation", 0);
+	servo_roll = settings->getValue(  "settings:servo_roll", 0);
+	
+	//start with screen off
+	dimmed = true;		
+	dimValue = 255;		
+
+	videoPlayer.loadVideos(se);
+}
+
+void machine::update() {
+	updateHeadtracking();
+	videoPlayer.update();
+	vidGrabberLeft.update();
+	if (videoPlayer.something_is_playing) drawFromVideo();
+	else drawFromCamera();
+}
+
+void machine::drawInFbo(ofImage * img, ofFbo* fbo) {
+	ofVec2f distance = getDistance();
+	img->setAnchorPercent(0.5,0.5);
+	fbo->begin();					
+	ofBackground(0);							
+	ofTranslate(fbo->getWidth()/2, fbo->getHeight()/2); //move to fbo center							
+	if (servo_roll == SERVO_ROLL_OFF) ofRotate(ofRadToDeg(roll-rx_roll)); 	
+	ofPushMatrix();			    
+	//tex->draw(ipd + distance.x*speed, -distance.y*speed-calibration, camWidth*zoom, camHeight*zoom);
+	img->draw(distance.y*speed, distance.x*speed, camWidth*zoom, camHeight*zoom);		
+	ofPopMatrix();											
+	fbo->end();
+	ofSetColor(255);
+}
+
+ofVec2f machine::getDistance() {
+	if (setup_type == TWO_WAY_SWAP) {			
+		ofVec2f self = ofVec2f(pitch, yaw);
+		ofVec2f other = ofVec2f(rx_pitch, rx_yaw);		
+		return self - other;
+	}
+	else if (setup_type == ONE_WAY_SWAP) {
+		if(!videoPlayer.something_is_playing) return ofVec2f(0,0);
+		else return ofVec2f(pitch,yaw);
+	}	
+}
+
+void machine::drawVideo() {				
+	dim();
+	if (camera_type == MONO) { //drawing the videograbber once in each fbo doesn't work, drawing the left fbo twice instead
+		fboLeft.draw(-ipd + ofGetWidth()/4, ofGetHeight()/2); //draw left	
+		fboLeft.draw(ipd + 3*ofGetWidth()/4, ofGetHeight()/2); //draw right
+	} else {
+	     if(swapLR == 0) {
+			 fboLeft.draw(ofGetWidth()/4, ofGetHeight()/2); //draw left.		 
+			 fboRight.draw(3*ofGetWidth()/4, ofGetHeight()/2); //draw right	
+		 }
+		  if(swapLR == 1) {
+			 fboRight.draw(ofGetWidth()/4, ofGetHeight()/2); //draw right to the left.		 
+			 fboLeft.draw(3*ofGetWidth()/4, ofGetHeight()/2); //draw left to the right	
+		 }
+	}			
+	ofSetColor(255);
+}
+
+void machine::updateHeadtracking() {
+	//update headtracking
+	ovrTrackingState state = ovrHmd_GetTrackingState(hmd, 0);
+	Quatf pose = state.HeadPose.ThePose.Orientation;
+	pose.GetEulerAngles<Axis_Y, Axis_Z, Axis_X>(&yaw, &roll, &pitch); //rotation order affects gimbal lock.	
+}
+
+void machine::drawFromVideo() {
+	//draw in fbo from preloaded video	
+	drawInFbo(&videoPlayer.img, &fboLeft);
+	drawInFbo(&videoPlayer.img, &fboRight);	
+}
+
+void machine::drawFromCamera() {
+	//draw from camera
+	if (camera_type == MONO) {			
+		//vidGrabberLeft.update();			
+		ofImage* img = new ofImage(vidGrabberLeft.getPixelsRef());
+		drawInFbo(img, &fboLeft);
+		drawInFbo(img, &fboRight);													
+		delete img;
+	} else if (camera_type == STEREO) {
+		//This is a stub, not yet implemented
+		vidGrabberLeft.update();
+		vidGrabberRight.update();
+	} else if (camera_type == OVRVISION) {		
+		g_pOvrvision->PreStoreCamData();
+		right.loadData(g_pOvrvision->GetCamImage(OVR::OV_CAMEYE_RIGHT, OVR::OV_PSQT_NONE), 640, 480, GL_RGB);		
+		left.loadData(g_pOvrvision->GetCamImage(OVR::OV_CAMEYE_LEFT, OVR::OV_PSQT_NONE), 640, 480, GL_RGB);	
+	}
+		
+	ofSetColor(255);		
+}
+
+
+void machine::drawMonitor(ofxFenster* window) {	
+	if (dimmed==true) ofSetColor(75);				
+	//if no video is playing, show camera
+	if (!videoPlayer.something_is_playing) vidGrabberLeft.draw(window->getWidth()/2, window->getHeight()/2, -window->getWidth(), -window->getHeight());		
+	//if a video is playing, show video
+	else videoPlayer.img.draw(window->getWidth()/2, window->getHeight()/2, window->getWidth()*-0.7, window->getHeight()*-0.7);
+	ofSetColor(255);
+}
+
+void machine::debug() {		
+	string framerate = ofToString(ofGetFrameRate());		
+	ofDrawBitmapString("FPS : " + framerate, 10, 10);
+	ofDrawBitmapString("pitch : " + ofToString(ofRadToDeg(pitch)), 10,20); //10
+	ofDrawBitmapString("yaw   : " + ofToString(ofRadToDeg(yaw)), 10,30);
+	ofDrawBitmapString("roll  : " + ofToString(ofRadToDeg(roll)), 10,40);
+
+	ofDrawBitmapString("zoom : " + ofToString(zoom), 10, 50);
+	ofDrawBitmapString("speed   : " + ofToString(speed), 10, 60);
+	ofDrawBitmapString("alignment  : " + ofToString(alignment), 10, 70);		
+	ofDrawBitmapString("side : " + ofToString(ipd), 10, 80);	
+
+	ofDrawBitmapString("distance.x : " + ofToString(getDistance().x), 10, 90);	
+	ofDrawBitmapString("distance.y : " + ofToString(getDistance().y), 10, 100);	
+	
+	ofDrawBitmapString("swap L/R: " + ofToString(swapLR), 10, 110);
+	
+	if (dimmed) ofDrawBitmapString("oculus screen is OFF", 10, 120); 
+	else		ofDrawBitmapString("oculus screen is ON", 10, 120); 	
+	
+	ofDrawBitmapString("drift correction : " + ofToString(drift_correction), 10, 140);			
+
+	ofDrawBitmapString("dimValue " + ofToString(dimValue), 10, 160);		
+}
+
+void machine::dim() {	
+	int next;
+
+	//if headtracking outside of range or dimmed on
+	if (abs(ofRadToDeg(yaw)) > RANGE || dimmed ) next = 0;
+
+	//if headtracking in range and dimmed off
+	else if (abs(ofRadToDeg(yaw)) < RANGE && !dimmed) next = 255;			
+
+	//smooth transition between current and next
+	dimValue = dimValue + 0.05*(next-dimValue);
+	ofSetColor(dimValue);		
+}
+
+void machine::calibrate() {	
+	ovrHmd_RecenterPose(hmd);
+	drift_correction=0;	
+}
+
+float* machine::getHeadtracking(){
+	float* c;
+	c = new float[3];
+	c[0] = roll;
+	c[1] = pitch;
+	c[2] = yaw;
+	return c;
+}
+
+
+void machine::clear() {
+	/*pSensor.Clear();
+    pHMD.Clear();
+	pManager.Clear();  	
+	System::Destroy();	*/
+}
+
+machine::machine(void)
+{
+
+}
+
+machine::~machine(void)
+{
+	
+}
+
 
 	//These are the parameters for the polynomial warp function to correct for the Oculus Rift and Webcam Lenses. Proper values still to be found
     //kept as ref, but they need to be properly calibrated according to camera and lens used.
-	K0 = 1.0;
+	/*K0 = 1.0;
     K1 = 5.74;
     K2 = 0.27;
     K3 = 0.0;
@@ -64,53 +258,9 @@ void machine::setup(ofxXmlSettings * se)
     _h = 1.0f;
     as = 640.0f/480.0f;
 	DistortionXCenterOffset = 90;	        
-    hmdWarpShader.load("shaders/HmdWarpExp");
-	
-	if (hmd->Type == ovrHmd_DK2) { 
-		fboLeft.allocate(DK2_WIDTH/2, DK2_HEIGHT);
-		fboRight.allocate(DK2_WIDTH/2, DK2_HEIGHT);
-	}
-	else if(hmd->Type == ovrHmd_DK1) {
-		fboLeft.allocate(DK1_WIDTH/2, DK1_HEIGHT);
-		fboRight.allocate(DK1_WIDTH/2, DK1_HEIGHT);
-	}
-
-	
-	fboLeft.setAnchorPercent(0.5, 0.5);
-	fboRight.setAnchorPercent(0.5, 0.5);
-
-	//was used for experimenting with torchlight-like overlay, left here as ref for later
-	overlay.loadImage("pictures/overlay4.png");
-	overlay.resize(2000*1.25,2000);
-	overlay.setAnchorPercent(0.5, 0.5);   
-
-	zoom = settings->getValue("settings:zoom", 0.85);
-	speed = settings->getValue("settings:speed", 920);
-	alignment = settings->getValue("settings:alignment", 0);
-	ipd = settings->getValue("settings:ipd", 8);	
-	ps3_position = settings->getValue("settings:ps3_position", 0);
-	servo_roll = settings->getValue(  "settings:servo_roll", 0);
-	dimTimer = ofGetElapsedTimeMillis();
-	dimmed = false;		
-}
-
-void machine::update() {
-	ovrTrackingState state = ovrHmd_GetTrackingState(hmd, 0);
-	Quatf pose = state.HeadPose.ThePose.Orientation;
-	pose.GetEulerAngles<Axis_Y, Axis_Z, Axis_X>(&yaw, &roll, &pitch); //rotation order affects gimbal lock.
-
-	if (camera_type == MONO) {
-		vidGrabberLeft.update();
-	} else if (camera_type == STEREO) {
-		vidGrabberRight.update();
-	} else if (camera_type == OVRVISION) {		
-		g_pOvrvision->PreStoreCamData();
-		right.loadData(g_pOvrvision->GetCamImage(OVR::OV_CAMEYE_LEFT, OVR::OV_PSQT_NONE), 640, 480, GL_RGB);		
-		left.loadData(g_pOvrvision->GetCamImage(OVR::OV_CAMEYE_RIGHT, OVR::OV_PSQT_NONE), 640, 480, GL_RGB);	
-	}
-	
-	ofVec2f distance = getDistance();	
-	ofSetColor(255);
+    hmdWarpShader.load("shaders/HmdWarpExp");	
+	*/
+/*
 
 	fboLeft.begin();					
 		ofBackground(0);				
@@ -129,12 +279,13 @@ void machine::update() {
 				ofPushMatrix();
 					if (ps3_position == PS3_VERTI) { 
 						ofRotate(90);
-						vidGrabberLeft.draw(ipd -(-distance.x*speed), -distance.y*speed-calibration, camWidth*zoom, camHeight*zoom);	
-					}else{
+						//vidGrabberLeft.draw(ipd -(-distance.x*speed), -distance.y*speed-calibration, camWidth*zoom, camHeight*zoom);	
+						vidGrabberLeft.draw(ipd + distance.y*speed+calibration, distance.x*speed - alignment, camWidth*zoom, camHeight*zoom);						
+					}
+					else {
 						ofRotate(0);
 						vidGrabberLeft.draw(ipd + distance.y*speed+calibration, distance.x*speed - alignment, camWidth*zoom, camHeight*zoom);						
 					}
-
 					
 					//vidGrabberLeft.draw(distance.y*speed, distance.x*speed, camWidth*zoom, camHeight*zoom);
 				ofPopMatrix();
@@ -152,17 +303,20 @@ void machine::update() {
 			if (camera_type == STEREO) {				
 				vidGrabberRight.draw(distance.y*speed, - distance.x*speed - alignment, camWidth*zoom, camHeight*zoom);	
 			}
-			else if (camera_type == OVRVISION) {		
+			else if (camera_type == OVRVISION) {							
+					ofRotate(180, 0, 0, 1);
 					ofPushMatrix();
-						ofRotate(180, 0, 0, 1);
 						right.draw(- distance.y*speed, - distance.x*speed - alignment, camWidth*zoom, camHeight*zoom);	
-					ofPopMatrix();					
+					ofPopMatrix();	 				
 			} 
 			else { // mono
 				ofPushMatrix();
 					if (ps3_position == PS3_VERTI) { 
 						ofRotate(90);
-						vidGrabberLeft.draw(ipd -(-distance.x*speed), -distance.y*speed-calibration, camWidth*zoom, camHeight*zoom);
+						ofPushMatrix();
+							//vidGrabberLeft.draw(ipd -(-distance.x*speed), -distance.y*speed-calibration, camWidth*zoom, camHeight*zoom);
+						vidGrabberLeft.draw(ipd + distance.y*speed+calibration, distance.x*speed + alignment, camWidth*zoom, camHeight*zoom);	
+						ofPopMatrix();
 					}else{
 						vidGrabberLeft.draw(ipd + distance.y*speed+calibration, distance.x*speed + alignment, camWidth*zoom, camHeight*zoom);	
 					}
@@ -172,149 +326,7 @@ void machine::update() {
 			}
 		ofPopMatrix();			
 	fboRight.end();	
-}
-
-ofVec2f machine::getDistance() {
-	if (setup_type == TWO_WAY_SWAP) {			
-		ofVec2f self = ofVec2f(pitch, yaw);
-		ofVec2f other = ofVec2f(rx_pitch, rx_yaw);		
-		return self - other;
-	}
-	else if (setup_type == ONE_WAY_SWAP) {
-		return ofVec2f(0,0);
-	}	
-}
-
-void machine::drawVideo() {				
-	if (camera_type == MONO) { //drawing the videograbber once in each fbo doesn't work, drawing the left fbo twice instead
-		fboLeft.draw(-ipd + ofGetWidth()/4, ofGetHeight()/2); //draw left	
-		fboLeft.draw(ipd + 3*ofGetWidth()/4, ofGetHeight()/2); //draw right
-	} else {
-	     if(swapLR == 0) {
-			 fboLeft.draw(ofGetWidth()/4, ofGetHeight()/2); //draw left.		 
-			 fboRight.draw(3*ofGetWidth()/4, ofGetHeight()/2); //draw right	
-		 }
-		  if(swapLR == 1) {
-			 fboRight.draw(ofGetWidth()/4, ofGetHeight()/2); //draw right to the left.		 
-			 fboLeft.draw(3*ofGetWidth()/4, ofGetHeight()/2); //draw left to the right	
-		 }
-	}		
-	ofSetColor(255);
-	//dim();
- 	if (dimmed) {
-		ofSetColor(0);
-		ofRect(0,0,ofGetWidth(), ofGetHeight());
-	}
-	ofSetColor(255);
-}
-
-void machine::drawMonitor() {	
-	if (dimmed==true){
-		ofSetColor(75);
-		fboLeft.draw(ofGetWidth()/2, ofGetHeight()/2);	
-	} else {
-		fboLeft.draw(ofGetWidth()/2, ofGetHeight()/2);	
-	}
-	ofSetColor(255);
-}
-
-void machine::debug() {	
-	
-	string framerate = ofToString(ofGetFrameRate());		
-	ofDrawBitmapString("FPS : " + framerate, 10, 10);
-	ofDrawBitmapString("pitch : " + ofToString(ofRadToDeg(pitch)), 10,20); //10
-	ofDrawBitmapString("yaw   : " + ofToString(ofRadToDeg(yaw)), 10,30);
-	ofDrawBitmapString("roll  : " + ofToString(ofRadToDeg(roll)), 10,40);
-
-	ofDrawBitmapString("zoom : " + ofToString(zoom), 10, 50);
-	ofDrawBitmapString("speed   : " + ofToString(speed), 10, 60);
-	ofDrawBitmapString("alignment  : " + ofToString(alignment), 10, 70);		
-	ofDrawBitmapString("side : " + ofToString(ipd), 10, 80);	
-
-	ofDrawBitmapString("distance.x : " + ofToString(getDistance().x), 10, 90);	
-	ofDrawBitmapString("distance.y : " + ofToString(getDistance().y), 10, 100);	
-	
-	ofDrawBitmapString("swap L/R: " + ofToString(swapLR), 10, 110);
-	if (dimmed == true) {
-		ofDrawBitmapString("oculus screen is OFF", 10, 120); 
-	} else if (dimmed == false) {
-		ofDrawBitmapString("oculus screen is ON", 10, 130); 
-	}
-	
-	ofDrawBitmapString("drift correction : " + ofToString(calibration), 10, 140);	
-
-	//ofDrawBitmapString("tracking caps " + ofToString(hmd->TrackingCaps), 10, 120);
-	//ofDrawBitmapString("yaw drift correction : " + ofToString(hmd->TrackingCaps && 0x001), 10, 130);
-	//ofDrawBitmapString("yaw drift correction : " + ofToString(hmd->TrackingCaps % 2), 10, 140);
-
-}
-
-void machine::drawOverlay() {
-		
-}
-
-void machine::dim() {
-	int timeDim = ofGetElapsedTimeMillis() - dimTimer;
-	//ofDrawBitmapString("alpha  : " + ofToString(ofMap(timeDim,0,2000,0,255)), 10,100);	
-	ofSetColor(0);
-	if (timeDim < 2000) {//if dim/undim was triggered less than 2 seconds ago
-		if (dimmed == true) { //if we must dim the lights			
-			ofSetColor(0,ofMap(timeDim,0,2000,0,255));			
-			ofRect(0,0,ofGetWidth(),ofGetHeight());
-			ofCircle(ofGetMouseX(),ofGetMouseY(),50);
-		}
-		else { //if we must turn the lights back on;
-			ofSetColor(255);
-			ofDrawBitmapString("alpha  : " + ofToString(ofMap(timeDim,0,2000,255,0)), 10,100);	
-			ofSetColor(0,ofMap(timeDim,0,2000,255,0));			
-			ofRect(0,0,ofGetWidth(), ofGetHeight());
-			ofCircle(ofGetMouseX(),ofGetMouseY(),50);
-		}
-	}
-	else if (dimmed == true) { // stay dark
-		ofRect(0,0,ofGetWidth(), ofGetHeight());
-		ofCircle(ofGetMouseX(),ofGetMouseY(),50);
-	}		
-	ofSetColor(255);
-//	ofCircle(ofGetMouseX(),ofGetMouseY(),50);
-}
-
-void machine::triggerDim() {
-	dimTimer = ofGetElapsedTimeMillis();
-	dimmed = !dimmed;	
-}
-
-void machine::calibrate() {	
-	ovrHmd_RecenterPose(hmd);
-	calibration=0;
-	//pitch_cal = pitch;
-}
-
-float* machine::getHeadtracking(){
-	float* c;
-	c = new float[3];
-	c[0] = roll;
-	c[1] = pitch;
-	c[2] = yaw;
-	return c;
-}
-
-void machine::clear() {
-	/*pSensor.Clear();
-    pHMD.Clear();
-	pManager.Clear();  	
-	System::Destroy();	*/
-}
-
-machine::machine(void)
-{
-
-}
-
-machine::~machine(void)
-{
-	
-}
+*/
 
 /*
 	// Configure Stereo settings.
